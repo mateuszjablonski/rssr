a0674b2a503bf2b3db631bc4dde68d5e5f4a3510
Mateusz Jabłoński <mateusz.piotr.jablonski@gmail.com>
2022-02-21 08:54:47 +0100

    Extracted variables, renamed articles to posts - dlapilota

--- dlapilota.php
+++ dlapilota.php
@@ -7,46 +7,52 @@ if ($php_version < 8) {
 require 'vendor/autoload.php';
 use IvoPetkov\HTML5DOMDocument;
 
+// initial variables
+$title = 'DlaPilota.pl';
 $base_url = 'https://dlapilota.pl';
+$logo_url = 'https://sklep.dlapilota.pl/img/dla-pilota-logo-1615743786.jpg';
+$description = 'Najpopularniejsze źródło informacji na temat małego i dużego lotnictwa';
+
+// extract posts as DOM objects
 $document_url = $base_url . '/wiadomosci';
 $document = new HTML5DOMDocument();
 $document->loadHTMLFile($document_url);
 $main = $document->querySelector('#block-dlapilota-theme-content');
-$articles = $main->querySelectorAll('.art-box');
-$logo_url = 'https://dlapilota.pl/themes/custom/dlapilota/logo.svg';
+$posts = $main->querySelectorAll('.art-box');
 
+// create channel root
 $rss = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0" />');
 $channel = $rss->addChild('channel');
-$channel->addChild('title', 'Dla Pilota');
+$channel->addChild('title', $title);
 $channel->addChild('link', $base_url);
-$channel->addChild('description', 'Najpopularniejsze źródło informacji na temat małego i dużego lotnictwa');
+$channel->addChild('description', $description);
 $logo = $channel->addChild('image');
-$logo->addChild('title', 'DlaPilota.pl logo');
 $logo->addChild('url', $logo_url);
-$logo->addChild('link', $logo_url);
+$logo->addChild('title', $title);
+$logo->addChild('link', $base_url);
 
-foreach ($articles as $article) {
+foreach ($posts as $post) {
 
-    // extract data from html
-    $title = $article
+    // extract data from each html post
+    $title = $post
         ->querySelector('.field--name-node-title h2 a')
         ?->innerHTML;
-    $url = $article
+    $url = $post
         ->querySelector('.field--name-node-title h2 a')
         ?->getAttribute('href');
-    $description = $article
+    $description = $post
         ->querySelector('.field--name-field-text')
         ?->innerHTML;
-    $image_src = $article
+    $image_src = $post
         ->querySelector('.field--name-field-media-image a img')
         ?->getAttribute('src');
-    $category = $article
+    $category = $post
         ->querySelector('.field--name-field-category-news a')
         ?->innerHTML;
-    $subcategory = $article
-        ->querySelector('.field--name-field-category-article div span')
+    $subcategory = $post
+        ->querySelector('.field--name-field-category-post div span')
         ?->innerHTML;
-    $date = $article
+    $date = $post
         ->querySelector('.field--name-node-post-date span')
         ?->innerHTML;
     
@@ -76,7 +82,7 @@ if (isset($_GET['debug'])) {
     $preview->preserveWhiteSpace = false;
     $preview->formatOutput = true;
     $preview->loadXML($rss_xml);
-    echo "<pre>" . htmlentities($preview->saveXML())."</pre>";
+    echo '<pre>' . htmlentities($preview->saveXML()) . '</pre>';
 } else {
     header('Content-Type: application/rss+xml');
     echo $rss_xml;
